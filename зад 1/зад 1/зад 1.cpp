977aa5ded8346ba27e86c3196b8a87e418e8126f
Grushik69 <grushevich.egor2005@gmail.com>
2024-05-15 13:13:09 +0300

    init: start

---
+++ зад 1/зад 1/зад 1.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+/*Дан динамический массив целых чисел {ai} (i=1, 2, ...,n), целые c и d  (c < d).
+Написать программу вычисления количества компонентов массива,  значения которых принадлежат отрезку [c, d].
+Для обращение к элементам  массива использовать указатель */
+int main() {
+
+	setlocale(0, "ru");
+
+	int n;
+	cout << "Введите количество элементов в массиве: ";
+	cin >> n;
+	int* arr = new int[n];
+	int* a = arr;
+	for (int i = 0; i < n; ++i) {
+		*a = i + 1;
+		cout << *a++ << ' ';
+	}cout << endl;
+	int c, d;
+	cout << "Введите c: ";
+	cin >> c;
+	cout << "Введите d: ";
+	cin >> d;
+	int count = 0;
+	a = arr;
+	for (int i = 0; i < n; ++i) {
+		if (*a >= c && *a <= d) {
+			count++;
+		}*a++;
+	}cout << "Элементы, которые находятся в отрезке: " << endl << count << endl;
+	delete[] arr;
+	return 0;
+
+}
